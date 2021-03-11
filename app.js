1521cbd874e15f625ea0df7dc38a4efd3422f679
DANISH <65593339+khand420@users.noreply.github.com>
2021-03-11 09:03:14 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,88 @@
+const express = require("express");
+const app = express();
+const path = require('path');
+const mongoose = require('mongoose');
+// const fs = require('fs');
+// const https = require('https');
+const pug = require('pug');
+
+// For SSL
+// var options = {
+//     key: fs.readFileSync('/etc/letsencrypt/live/example.com/privkey.pem'),
+//     cert: fs.readFileSync('/etc/letsencrypt/live/example.com/cert.pem'),
+//     ca: fs.readFileSync('/etc/letsencrypt/live/example.com/chain.pem')
+// };
+
+//Creating socket.io server
+// const server = https.createServer(options).listen(8000);
+const io = require('socket.io')(8000);
+
+// mongoose.connect('mongodb://localhost/', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect('mongodb://localhost/ichat', { useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection;
+const nameSchema = new mongoose.Schema({
+    name: String
+});
+app.listen(5001,()=>{
+    console.log("Server is running on port 5001");
+});
+const namemodel = mongoose.model('username', nameSchema);
+
+app.use(express.static(__dirname + '/static'));
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, "views"));
+
+app.get("/", (req, res) => {
+    namemodel.find({}, (err, username) => {
+        if (err) return console.error(err);
+
+        let jsonobj = JSON.stringify(username);
+        res.status(200).render("main.pug", { "mongodoc": jsonobj });
+    });
+});
+
+const users = {};
+
+// Handling events with Socket.io
+io.on('connection', (socket) => {
+    socket.on('new-user-joined', username => {
+        let nameobj = {
+            name: username
+        };
+
+        const name = new namemodel(nameobj);
+        name.save().then(() => {
+            users[socket.id] = username;
+            namemodel.find({}, (err, namedocument) => {
+                if (err) return console.error(err);
+
+                socket.broadcast.emit("user-joined", namedocument);
+            });
+        }).catch((err) => {
+            console.error(err);
+        });
+    });
+
+    socket.on("send", message => {
+        socket.broadcast.emit("receive", { message: message, name: users[socket.id] });
+    });
+
+    socket.on("disconnect", () => {
+        namemodel.deleteOne({ name: users[socket.id] }).then(() => {
+            namemodel.find({}, (err, newdocument) => {
+                if (err) return console.error(err);
+
+                let newlist = {
+                    'name': users[socket.id],
+                    'onlinelist': newdocument
+                };
+
+                socket.broadcast.emit("user-left", newlist);
+                delete users[socket.id];
+            });
+        }).catch((err) => {
+            console.error(err);
+        });
+    });
+});
+
